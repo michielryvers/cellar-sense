8650d9695c5a3c12127fe61cbf491cc5a91c2087
Michiel Ryvers <mryvers@samenferm.be>
2025-05-16 21:42:28 +0200

    Add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/cellar-sense/",
   plugins: [vue(), tailwindcss()],
 });
